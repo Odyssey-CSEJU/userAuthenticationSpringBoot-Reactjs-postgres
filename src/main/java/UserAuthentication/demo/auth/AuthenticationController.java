a2dad2434e6933766d1c6177b086fbc3779061e4
RubayedMunna <jucse29.370@gmail.com>
2024-09-02 16:00:54 +0600

    worked on CORS

--- src/main/java/UserAuthentication/demo/auth/AuthenticationController.java
+++ src/main/java/UserAuthentication/demo/auth/AuthenticationController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
@@ -19,7 +24,9 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.register(request));
         //
     }
-        @PostMapping("/authenticate")
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ){
